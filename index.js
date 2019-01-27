dd48b4dfbbc27cf09c056dd331cdcd9cc5eedb12
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2019-01-27 08:08:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,34 @@
-"use strict";
-
 var Service, Characteristic;
-var request = require("request");
 
-module.exports = function (homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-
-    homebridge.registerAccessory("homebridge-stateless-switch", "StatelessSwitch", StatelessSwitch);
-};
-
-function StatelessSwitch(log, config) {
-    this.log = log;
-    this.name = config.name;
-
-    this.onUrl = config.onUrl;
-
-    this.homebridgeService = new Service.Switch(this.name);
-    this.homebridgeService.getCharacteristic(Characteristic.On)
-        .on("get", this.getStatus.bind(this))
-        .on("set", this.setStatus.bind(this));
+module.exports = function(homebridge) {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	homebridge.registerAccessory("homebridge-stateless-switch", "StatelessSwitch", StatelessSwitch);
 }
 
-StatelessSwitch.prototype = {
+function StatelessSwitch(log, config) {
 
-    identify: function (callback) {
-        this.log("Identify requested!");
-        callback();
-    },
+	this.log = log;
 
-    getServices: function () {
-        return [this.homebridgeService];
-    },
+	this.name = config["name"];
+	this.sn = config['sn'] || 'Unknown';
 
-    getStatus: function (callback) {
-        callback(null, false);
-    },
+	this.service = new Service.StatelessProgrammableSwitch();
 
-    setStatus: function (on, callback) {
-        this.makeSetRequest(true, callback);
-    },
+	var self = this;
 
-    makeSetRequest: function (on, callback) {
-        var statelessSwitch = this;
-        var onUrl = this.onUrl;
+//		self.service.getCharacteristic(Characteristic.ProgrammableSwitchEvent).setValue(self.value);
+}
 
-        // if (onUrl.length === 0) {
-        //     this.log("Ignoring setStatus() request 'offUrl' or 'onUrl' is not defined");
-        //     callback(new Error("No 'onUrl' defined!"));
-        //     return;
-        // }
+StatelessSwitch.prototype.getServices = function() {
 
-        this._httpRequest(onUrl, function (error, response, body) {
-            if (error) {
-                statelessSwitch.log("setStatus() failed: %s", error.message);
-                statelessSwitch.resetSwitchWithTimeout();
-                callback(error);
-            }
-            else if (response.statusCode !== 200) {
-                statelessSwitch.log("setStatus() http request returned http error code: %s", response.statusCode);
-                statelessSwitch.resetSwitchWithTimeout();
-                callback(new Error("Got html error code " + response.statusCode));
-            }
-            else {
-                statelessSwitch.log("setStatus() successfully set switch to %s", on? "ON": "OFF");
-                statelessSwitch.resetSwitchWithTimeout();
-                callback(undefined, body);
-            }
-        }.bind(this));
-    },
+	var informationService = new Service.AccessoryInformation();
 
-    resetSwitchWithTimeout: function () {
-        this.log("Resetting switch to OFF");
-        setTimeout(function () {
-            this.homebridgeService.setCharacteristic(Characteristic.On, false);
-        }.bind(this), 1000);
+	informationService
+		.setCharacteristic(Characteristic.Name, this.name)
+		.setCharacteristic(Characteristic.Manufacturer, "Thomas Nemec")
+		.setCharacteristic(Characteristic.Model, "Stateless Switch")
+		.setCharacteristic(Characteristic.SerialNumber, this.sn);
 
-    },
-
-    _httpRequest: function (url, callback) {
-        request(url,
-            function (error, response, body) {
-                callback(error, response, body);
-            }
-        )
-    }
+	return [informationService, this.service];
 }
